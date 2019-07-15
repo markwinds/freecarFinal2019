3be709948411eb1c56fbdaf8c2f0997bf6bb3f51
markwinds <38316819+markwinds@users.noreply.github.com>
2019-07-15 18:17:36 +0800

    改了叶廉的电磁通道

--- App/Inc/electromagnetic.h
+++ App/Inc/electromagnetic.h
@@ -11,7 +11,7 @@
 /*------------------------------------------------define------------------------------------------------*/
 #define ADC_LEFT ADC0_DP1 //需要根据实际情况更改
 #define ADC_RIGHT ADC1_DM1
-#define ADC_MIDDLE ADC0_DP3
+#define ADC_MIDDLE ADC0_DM1
 /* 
     horizontal_middle = ADC0_DP3,
     horizontal_left = ADC0_DP1,
@@ -21,11 +21,12 @@
 */
 
 /*------------------------------------------------Perdefine Type---------------------------------------------*/
-typedef struct MedianTree{
+typedef struct MedianTree
+{
     uint8 history[64];
     uint8 tree[256];
     uint8 hvHead;
-}MedianTree;
+} MedianTree;
 /*------------------------------------------------外部变量---------------------------------------------*/
 extern int AD_val_min[3];
 extern int AD_val_max[3];
@@ -35,14 +36,14 @@ extern int dis_AD_val[3];
 extern int disgy_AD_val[3];
 /*--------------------------------------------------函数--------------------------------------------------*/
 /*-------------------------domestic函数-------------------------*/
-void TreeInsert(uint8* ,int, int, int, int);
-void TreeDelete(uint8* ,int, int, int, int);
-int GetMedian(uint8* ,int, int, int, int);
+void TreeInsert(uint8*, int, int, int, int);
+void TreeDelete(uint8*, int, int, int, int);
+int  GetMedian(uint8*, int, int, int, int);
 
 void InitTree(struct MedianTree*, ADCn_Ch_e);
 
 uint8 Normalized(int);
 /*-------------------------abroad函数-------------------------*/
-extern int ReadValue(ADCn_Ch_e);
+extern int  ReadValue(ADCn_Ch_e);
 extern void InitEM();
 #endif
